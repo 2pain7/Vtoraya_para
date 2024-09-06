654c9727efa8f1665d5cb1133918cac949b85043
2pain7 <dimitriepesic@gmail.com>
2024-09-06 09:05:30 +0500

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double sum = 0;
+            for (double i = 1; i <= 10; i++)
+            {
+                sum += Math.Pow(x, i) / (x + i);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
